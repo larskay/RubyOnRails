d0489efa4252d619c6e5617d5c1386c5cdc5db58
Lars Antonio Kay <antlak@LINUX-PC.(none)>
2013-05-23 15:57:26 +0200

    Finished product

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,12 @@
 class StoreController < ApplicationController
+  skip_before_action :authorize
+  include CurrentCart
+  before_action :set_cart
   def index
-    @products = Product.order(:title)
-    @counter = session[:counter]
-    @counter.nil? ? @counter = 1 : @counter+=1
-    session[:counter] = @counter
+  	if params[:set_locale]
+  		redirect_to store_url(locale: params[:set_locale])
+	else
+    	@products = Product.order(:title)
+	end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,11 @@ Depot::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  #config mail options
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :test
+  end
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
